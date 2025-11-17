d0449a9960d93e25b2a5167951ed6253f0ba650f
CraigC830 <craigcat830@gmail.com>
2025-11-17 01:37:58 -0500

    Progress on GUI

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,19 +1,21 @@
 package program;
 
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
-import java.awt.*;
 
 public class Main extends JFrame {
 
     private JPanel DonutGuiPanel;
     private JLabel ShopName;
-    private JLabel FilterLable;
     private JLabel MenuLable;
     private JLabel OrderLable;
-    private JList list1;
-    private JTable table1;
+    private JTable OrderTable;
+    private JScrollPane OrderScrollPane;
+    private JList MenuList;
+    private JScrollPane MenuScrollPane;
+    private JButton OrderButton;
+    private JSpinner QTYspinner;
+    private JLabel QTYLable;
 
     public Main() {
         setContentPane(DonutGuiPanel);
@@ -22,9 +24,30 @@ public class Main extends JFrame {
         setSize(800, 600);
         setLocationRelativeTo(null);
         setVisible(true);
-        
+
+        DefaultTableModel model = (DefaultTableModel) OrderTable.getModel();
+
+        // Add rows
+        model.addRow(new Object[]{"round", "red"});
+        model.addRow(new Object[]{"square", "green"});
+
     }
     public static void main(String[] args) {
         new Main();
+    }
+
+    private void createUIComponents() {
+        OrderTable = new JTable(
+                new DefaultTableModel(
+                        new Object [][] {
+
+                        },
+                        new String [] {
+                                "ID", "Item", "Options", "QTY", "Price", "Total"
+                        }
+                )
+
+        );
+
     }
 }
